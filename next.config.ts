97355771d726c263db4172273e48841fae695ee3
Aditya-fmh <adityafirmanshah09@gmail.com>
2025-05-16 09:42:00 +0700

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  experimental: {
+    // Enable caching for faster builds
+    turbotrace: {
+      logLevel: 'error'
+    }
+  }
 };
 
 export default nextConfig;
